9a231a82b2e9122c0c0c24ed5205611803f62581
Geovane-Araujo <geovane-araujo@outlook.com>
2024-12-18 22:19:39 -0300

    ajustes interceptior

--- src/main/resources/db/migration/V20241218080000002__Create_repository.sql
+++ src/main/resources/db/migration/V20241218080000002__Create_repository.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS repository(
 
      id uuid,
-     name varchar,
-     report uuid
+     name varchar
 );
 
 CREATE TABLE IF NOT EXISTS report(
@@ -16,5 +15,4 @@ CREATE TABLE IF NOT EXISTS report(
 ALTER TABLE repository  ADD CONSTRAINT pk_repository  PRIMARY KEY (id);
 ALTER TABLE report  ADD CONSTRAINT pk_report  PRIMARY KEY (id);
 
-ALTER TABLE repository ADD CONSTRAINT fk_repository_report_report FOREIGN KEY (report) REFERENCES report(id);
 ALTER TABLE report ADD CONSTRAINT fk_report_repository_repository FOREIGN KEY (repository) REFERENCES repository(id);
