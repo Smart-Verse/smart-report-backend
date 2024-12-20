8fb8f468943d4543632df40c1e4d8b2107cafc0a
Geovane-Araujo <geovane-araujo@outlook.com>
2024-12-19 22:15:59 -0300

    ajustes interceptior

---
+++ src/main/java/com/smartverse/smartreportbackend/handlers/reports/ReportHandlerImpl.java
@@ -0,0 +1,9 @@
+package com.smartverse.smartreportbackend.handlers.reports;
+
+import com.smartverse.smartreportbackend_gen.ReportEntity;
+import com.smartverse.smartreportbackend_gen.ReportHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportHandlerImpl extends ReportHandler {
+}
