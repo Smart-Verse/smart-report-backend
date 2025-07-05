ac310bdff3a8a9b2c6dad0a26a205604d45a48be
Geovane Araujo <geovane-araujo@outlook.com>
2025-07-04 22:12:12 -0300

    criado endpoint para salvar emails do beta

---
+++ src/main/java/com/smartverse/smartreportbackend/handlers/beta/ParticipateBetaImpl.java
@@ -0,0 +1,28 @@
+package com.smartverse.smartreportbackend.handlers.beta;
+
+import com.potatotech.authorization.stereotype.Anonymous;
+import com.smartverse.smartreportbackend.services.beta.ParticipateBetaService;
+import com.smartverse.smartreportbackend_gen.ParticipateBeta;
+import com.smartverse.smartreportbackend_gen.ParticipateBetaInput;
+import com.smartverse.smartreportbackend_gen.ParticipateBetaOutput;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class ParticipateBetaImpl implements ParticipateBeta {
+
+    @Autowired
+    ParticipateBetaService participateBetaService;
+
+
+    @Override
+    @Anonymous
+    public ResponseEntity<ParticipateBetaOutput> participateBeta(ParticipateBetaInput input) {
+        var output = new ParticipateBetaOutput();
+        output.result = participateBetaService.participateBeta(input.email, input.name);
+        return ResponseEntity.ok(output);
+    }
+}
---
+++ src/main/java/com/smartverse/smartreportbackend/services/beta/ParticipateBetaService.java
@@ -0,0 +1,31 @@
+package com.smartverse.smartreportbackend.services.beta;
+
+import com.mongodb.client.MongoCollection;
+import com.potatotech.authorization.tenant.TenantContext;
+import com.smartverse.smartreportbackend.common.FileCommon;
+import com.smartverse.smartreportbackend.config.mongo.ConnectionMongoDb;
+import com.smartverse.smartreportbackend_gen.ReportEntity;
+import org.bson.Document;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class ParticipateBetaService {
+
+    public boolean participateBeta(String email, String name){
+
+        var connection = ConnectionMongoDb.getInstance();
+        var database = connection.getDatabase();
+
+        MongoCollection<Document> collection = database.getCollection("beta");
+
+        var map = new HashMap<String, Object>();
+
+        map.put("name", email);
+        map.put("email", name);
+
+        collection.insertOne(new Document(map));
+        return true;
+    }
+}
