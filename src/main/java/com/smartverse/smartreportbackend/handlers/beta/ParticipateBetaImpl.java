faa8b11199c699c5806b0f24921401f51792db33
Geovane Araujo <geovane-araujo@outlook.com>
2025-07-05 22:30:58 -0300

    Ajustado cors

--- src/main/java/com/smartverse/smartreportbackend/handlers/beta/ParticipateBetaImpl.java
+++ src/main/java/com/smartverse/smartreportbackend/handlers/beta/ParticipateBetaImpl.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ParticipateBetaImpl implements ParticipateBeta {
 
     @Autowired
